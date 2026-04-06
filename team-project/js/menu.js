5d8e62effb03f9f56b0d25f67558d4c8a73f3394
Anastasiia <147639683+GoncharAnastasia@users.noreply.github.com>
2026-04-06 14:47:27 +0300

    Create menu.js

---
+++ team-project/js/menu.js
@@ -0,0 +1,24 @@
+const menuToggle = document.getElementById("menuToggle");
+const navRight = document.getElementById("navRight");
+const menuLinks = document.querySelectorAll("#mobileMenu a");
+
+if (menuToggle && navRight) {
+  menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    navRight.classList.toggle("open");
+  });
+
+  menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      menuToggle.classList.remove("active");
+      navRight.classList.remove("open");
+    });
+  });
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth >= 768) {
+      menuToggle.classList.remove("active");
+      navRight.classList.remove("open");
+    }
+  });
+}
