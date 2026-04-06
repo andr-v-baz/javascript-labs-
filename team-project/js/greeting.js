69851abbcc648043243cf6ebcfda597ef0fca978
Anastasiia <147639683+GoncharAnastasia@users.noreply.github.com>
2026-04-06 13:44:28 +0300

    Update greeting.js

--- team-project/js/greeting.js
+++ team-project/js/greeting.js
@@ -6,57 +6,56 @@ const button = document.getElementById("saveName");
 const inputBox = document.getElementById("nameInputBox");
 const editBtn = document.getElementById("editNameBtn");
 
-// якщо ім'я вже збережене
 const savedName = localStorage.getItem("username");
 
 if(savedName){
-showGreeting(savedName);
-inputBox.style.display = "none";
-editBtn.style.display = "inline-block";
+  showGreeting(savedName);
+  inputBox.style.display = "none";
+  editBtn.style.display = "inline-block";
 }
 
-button.addEventListener("click", ()=>{
+button.addEventListener("click", saveUserName);
 
-const name = input.value.trim();
-
-if(name !== ""){
-
-localStorage.setItem("username", name);
-
-showGreeting(name);
-
-inputBox.style.display = "none";
-editBtn.style.display = "inline-block";
-
-input.value = "";
-
-}
-
+input.addEventListener("keydown", (event)=>{
+  if(event.key === "Enter"){
+    saveUserName();
+  }
 });
 
 editBtn.addEventListener("click", ()=>{
-
-inputBox.style.display = "block";
-editBtn.style.display = "none";
-
+  inputBox.style.display = "block";
+  editBtn.style.display = "none";
+  input.focus();
 });
 
+function saveUserName(){
+  const name = input.value.trim();
+
+  if(name !== ""){
+    localStorage.setItem("username", name);
+    showGreeting(name);
+
+    inputBox.style.display = "none";
+    editBtn.style.display = "inline-block";
+
+    input.value = "";
+  }
+}
+
 function showGreeting(name){
+  const hour = new Date().getHours();
 
-const hour = new Date().getHours();
+  let text = "";
 
-let text = "";
+  if(hour < 12){
+    text = "Добрий ранок";
+  }
+  else if(hour < 18){
+    text = "Добрий день";
+  }
+  else{
+    text = "Добрий вечір";
+  }
 
-if(hour < 12){
-text = "Добрий ранок";
-}
-else if(hour < 18){
-text = "Добрий день";
-}
-else{
-text = "Добрий вечір";
-}
-
-greeting.textContent = text + ", " + name + "!";
-
+  greeting.textContent = `${text}, ${name}!`;
 }
